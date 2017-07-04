e900493b69cf9791473c5538ed1144a80d145f62
BoscoMurray <rossamurray@gmail.com>
2017-07-04 21:51:08 +0100

    word count button works on one page

--- app/src/main/java/com/example/user/week07_day02_word_counter/ActivityWordCount.java
+++ app/src/main/java/com/example/user/week07_day02_word_counter/ActivityWordCount.java
@@ -2,12 +2,40 @@ package com.example.user.week07_day02_word_counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityWordCount extends AppCompatActivity {
+
+    EditText enteredWords;
+    Button countButton;
+    TextView wordCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_count);
+
+        Log.d(getClass().toString(), "onCreate called");
+
+        enteredWords = (EditText) findViewById(R.id.word_list);
+        countButton = (Button) findViewById(R.id.count_button);
+    }
+
+    public void onCountButtonClicked(View button) {
+        String string = enteredWords.getText().toString();
+
+        Log.d(getClass().toString(), "onCountButtonClicked was called");
+        Log.d(getClass().toString(), "The string entered was'" + string + "'");
+
+        WordList wordlist = new WordList(string);
+        Integer count = wordlist.countWords();
+
+
+        wordCount = (TextView) findViewById(R.id.count_text);
+        wordCount.setText(count.toString());
     }
 }
